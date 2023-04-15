c69aff6e747f07ae84ec65dc4c97e71e014a9e80
alicelinkokoro <101160183+alicelinkokoro@users.noreply.github.com>
2023-04-15 12:08:45 -0400

    Create main.js

---
+++ exercises/Exercise07/main.js
@@ -0,0 +1,18 @@
+/* console.log ('this works');
+
+function  sayHello () {
+    console.log("Hi");
+}
+
+sayHello ()
+*/
+
+const fruits = ["apple", "orange","grapes"]
+
+console.log(fruits[1])
+
+const students = ["alice", "han", "chi chi", "brent"]
+for(let index = 0; index < students.length; index++) {
+    console.log ("Name of student is :" + students[index]);
+    index++;
+}
