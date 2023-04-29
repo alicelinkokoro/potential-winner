59a2aae911467c24da5fe26e989c57ad24e8cfd0
alicelinkokoro <101160183+alicelinkokoro@users.noreply.github.com>
2023-04-29 11:50:03 -0400

    Update main.js

--- projects/project03/main.js
+++ projects/project03/main.js
@@ -7,11 +7,25 @@ const records = [
     },
       {
         title: 'Last Leaf',
-        releaseYear: 2011,
+        releaseYear: 2017,
         artist: 'Danish String Quartet',
         link: '02Last Leaf.html',
     },
-  ]
+
+    {
+      title: 'Khmer',
+      releaseYear: 2019,
+      artist: 'Nils Petter Molvaer',
+      link: '03Khmer.html',
+  },
+     {
+     title: 'Changing Places',
+     releaseYear: 2003,
+     artist: 'Tord Gustavsen Trio',
+     link: '04Changing Places.html',
+  },
+]
+
   
   const recordThumbnail = document.querySelector('.record_thumbnail');
   
